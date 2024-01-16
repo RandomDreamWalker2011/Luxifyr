ec880e07e73b5b9df0a23ff9ca07a301cd2ec4a0
RandomDreamWalker 97062 @ENamnor <104535753+RandomDreamWalker2011@users.noreply.github.com>
2024-01-16 23:04:23 +0000

    Commit Setup 1#

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+
+
+class Luxifyr_Interpreter:
+    def __init__(self, ):
+        self.variables = {}
+
+    def run(self, file):
+        tokens = file.splitline("\n")
+        for token in tockens:
+            print("token")
