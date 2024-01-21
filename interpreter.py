56219c83113c4effc02fdfb36e705b3a5bc02639
RandomDreamWalker 97062 @ENamnor <104535753+RandomDreamWalker2011@users.noreply.github.com>
2024-01-20 18:55:01 -0600

    Create interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,17 @@
+# INTERPRETER
+from setup import LuxifyrInterpreter as LuxI
+interpreter = LuxI()
+
+
+file_path = 'entry.lux'
+
+try:
+    with open(file_path, 'r') as file:
+        # Read the entire content
+        content = file.read()
+except FileNotFoundError:
+    print(f"The file '{file_path}' does not exist.")
+
+interpreter = interpreter.run(content)
+if interpreter is not None:
+    pass
