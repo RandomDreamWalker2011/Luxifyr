afbad704e59b640132f32c5ffcfdb46bed815f09
RandomDreamWalker 97062 @ENamnor <104535753+RandomDreamWalker2011@users.noreply.github.com>
2024-01-23 17:04:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 # setup.py
+Tokens = {}
+
+
+class Token:
+    name = None
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+        self.variable = False
+
+    def set_variable(self, name):
+        self.name = name
+        self.variable = True
 
 
 class LuxifyrInterpreter:
-    def __init__(self, ):
+    def __init__(self):
         self.variables = {}
 
     @staticmethod
     def run(file):
-        tokens = file.splitlines("\n")
+        tokens = file.split()
         for token in tokens:
             print(token)
-            
+
     @staticmethod
     def breakdown(tokens):
-        for token in tokens:
-            starts = []
-            # mainfunc = []
-            for char in token:
-                if char == ":":
-                    if starts[0] or starts[1]:
-                        starts.append(char)
-                    elif starts[3]:
-                        pass
+        pass
